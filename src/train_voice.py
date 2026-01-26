2bd8bee6df63bac730b00f3fece193211999343f
StroepWafel <109832156+StroepWafel@users.noreply.github.com>
2026-01-26 21:05:49 +1030

    logic and stuff

--- src/train_voice.py
+++ src/train_voice.py
@@ -11,35 +11,56 @@ voice_name = input("Please enter the name of the voice to create:\n")
 
 print(f"Creating voice '{voice_name}'")
 
-ref_audio = "mic.wav"
-ref_text  = "Quick brown foxes jump over lazy dogs while musicians play jazzy tunes in the quiet park."
+using_file = input("Are you using a file or recording audio (file/recording)?\n")
 
-# Default input device
-device_index = sd.default.device[0]
-print("Using default input device:", sd.query_devices(device_index)["name"])
+print("Using File" if using_file == "file" else "recording audio")
 
-print("Please read the following text aloud (press enter when ready): \n" + ref_text)
+if (using_file == "file"):
+    ref_audio = input("Please enter the name of the .wav audio file:\n") + ".wav"
+    print(f"Loading file '{ref_audio}'")
 
-input()
+    ref_text  = input("Please enter the transcript for the audio:\n")
 
-print("Recording... press Enter to stop")
+    print(f"transcript '{ref_text}'")
+else:
+    # Default input device
+    device_index = sd.default.device[0]
+    ref_text  = "Quick brown foxes jump over lazy dogs while musicians play jazzy tunes in the quiet park."
 
-# List to hold chunks
-audio_chunks = []
+    print("Using default input device:", sd.query_devices(device_index)["name"])
+    
+    print("Please read the following text aloud (press enter when ready): \n" + ref_text)
 
-def callback(indata, frames, time, status):
-    audio_chunks.append(indata.copy())
+    input()
 
-# Open stream
-with sd.InputStream(samplerate=SAMPLE_RATE, channels=CHANNELS, callback=callback):
-    input()  # Wait for user to press Enter
+    print("Recording... press Enter to stop")
 
-# Combine chunks into a single array
-audio_data = np.concatenate(audio_chunks, axis=0)
+    # List to hold chunks
+    audio_chunks = []
 
-# Save to file
-sf.write("mic.wav", audio_data, SAMPLE_RATE)
-print("Saved mic.wav")
+    def callback(indata, frames, time, status):
+        audio_chunks.append(indata.copy())
+
+    # Open stream
+    with sd.InputStream(samplerate=SAMPLE_RATE, channels=CHANNELS, callback=callback):
+        input()  # Wait for user to press Enter
+
+    # Combine chunks into a single array
+    audio_data = np.concatenate(audio_chunks, axis=0)
+
+    # Save to file
+    sf.write("mic.wav", audio_data, SAMPLE_RATE)
+    print("Saved mic.wav")
+
+    model = Qwen3TTSModel.from_pretrained(
+        "Qwen/Qwen3-TTS-12Hz-1.7B-Base",
+        device_map="cuda:0",
+        dtype=torch.bfloat16,
+        attn_implementation="sdpa",
+    )
+
+    ref_audio = "mic.wav"
+    ref_text  = "Quick brown foxes jump over lazy dogs while musicians play jazzy tunes in the quiet park."
 
 model = Qwen3TTSModel.from_pretrained(
     "Qwen/Qwen3-TTS-12Hz-1.7B-Base",
@@ -47,9 +68,6 @@ model = Qwen3TTSModel.from_pretrained(
     dtype=torch.bfloat16,
     attn_implementation="sdpa",
 )
-
-ref_audio = "mic.wav"
-ref_text  = "Quick brown foxes jump over lazy dogs while musicians play jazzy tunes in the quiet park."
 
 # Generate a "voice clone prompt" — this holds the speaker embedding
 prompt_items = model.create_voice_clone_prompt(
